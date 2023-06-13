bded521ffa1fbb912a60c14602d22a8a064cd184
Rose <i.a.rose.bk@gmail.com>
2023-06-13 14:33:23 -0400

    more updates

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const { createRandomUser } = require("./FakerInfoFile");
 const { writeJSONFile, readJSONFile } = require("./Assistantfile");
 function run() {
-    let products = readJSONFile("./data", "Fakerfakedata.json");
-    console.log("Products Index file", products);
-    // if (process.argv[3]) {
-    //   products.push(...randomProductFactory(process.argv[3]));
-    // } else {
-    products.push(createRandomUser());
-    // }
-    writeJSONFile("./data", "Fakerfakedata.json", products);
+    
 }
 
 run();
